63e91d27c86842d7cf7247723261e5cba210fac6
HaneenIbrahem <haneen.m.ibrahem@gmail.com>
2023-11-03 16:07:34 +0200

    update

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    supportFile: "cypress/support/commands.ts",
+    baseUrl: 'https://opensource-demo.orangehrmlive.com',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
